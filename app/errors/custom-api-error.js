a458fcedacfd16c58cb86d569513fd3ae8d79c02
Johandika <johanelyosse@gmail.com>
2023-11-07 17:25:43 +0700

    first commit

---
+++ app/errors/custom-api-error.js
@@ -0,0 +1,11 @@
+// // Membuat kelas CustomAPIError sebagai turunan dari kelas Error(Error adalah kelas bawaan JS)
+// class CustomAPIError extends Error {
+//   // Membuat constructor dengan parameter message
+//   constructor(message) {
+//     // Memanggil constructor dari kelas Error dengan parameter message yang diterima
+//     super(message);
+//   }
+// }
+
+// // Menjadikan kelas CustomAPIError dapat diakses dari file JavaScript lain
+// module.exports = CustomAPIError;
---
+++ app/errors/unauthenticated.js
@@ -0,0 +1,11 @@
+// const { StatusCodes } = require("http-status-codes");
+// const CustomAPIError = require("./custom-api-error");
+
+// class Unauthenticated extends CustomAPIError {
+//   constructor(message) {
+//     super(message);
+//     // memberikan statusCode not found
+//     this.statusCode = StatusCodes.UNAUTHORIZED;
+//   }
+// }
+// module.exports = Unauthenticated;
